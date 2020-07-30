650e49accb796663f0ee4e39a8c8c4b1e9aac837
juan251189 <juan2511@hotmail.com>
2020-07-31 02:45:04 +1000

    #day3
    simple styles added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Vue<span>CORDION</span></h1>
+    </header>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FAQ from './FAQ';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    FAQ
+  },
+  data(){
+    return{
+      faqs:[{
+        question:'',
+        answer:'',
+        open:false
+
+      }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  background-color: #EEE;
+  font-family: sans-serif;
+}
+
+header{
+  background-color: #3c3c3c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+}
+header h1{
+  color:#EEE;
+  font-size: 28px;
+  font-weight: 300;
+  text-transform: uppercase;
+}
+
+header h1 span{
+  color:#56e3b8;
+  font-weight: 900;
 }
 </style>
---
+++ src/FAQ.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="faq">
+    <div class="question">{{faq.question}}</div>
+    <div class="answer">{{faq.answer}}</div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'FAQ',
+    props:['faq','i']
+  }
+</script>
